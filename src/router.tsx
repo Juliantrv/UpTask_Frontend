3f69343328970a7f7d7792f3778bc95b4ef02c49
Julian <julian.trujillo@focunti.com>
2025-05-26 20:33:29 -0500

    fix: render the app

--- src/router.tsx
+++ src/router.tsx
@@ -1,51 +1,52 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppLayout from "@/layouts/AppLayout"
-import AtuhLayout from "@/layouts/AtuhLayout";
-import ProfileLayout from "@/layouts/ProfileLayout"
-import DashboardView from "@/views/DashboardView"
-import CreateProjectView from "@/views/projects/CreateProjectView"
-import EditProjectView from "@/views/projects/EditProjectView"
-import ProjectsDetailsView from "@/views/projects/ProjectsDetailsView"
-import LoginView from "@/views/auth/LoginView"
-import RegisterView from "@/views/auth/RegisterView"
-import ConfirmAccountView from "@/views/auth/ConfirmAccountView"
-import RequestNewCodeView from "@/views/auth/RequestNewCodeView"
-import ForgotPasswordView from "@/views/auth/ForgotPasswordView"
-import NewPasswordView from "@/views/auth/NewPasswordView"
-import ProjectTeamView from "@/views/projects/ProjectTeamView"
-import ProfileView from "@/views/profile/ProfileView"
-import ChangePasswordView from "@/views/profile/ChangePasswordView"
-import NotFound from "@/views/404/NotFound"
+import AppLayout from "@/layouts/AppLayout";
+import AuthLayout from "@/layouts/AuthLayout";
+import ProfileLayout from "@/layouts/ProfileLayout";
+const DashboardView = lazy(() => import("@/views/DashboardView"));
+const CreateProjectView = lazy(() => import("@/views/projects/CreateProjectView"));
+const EditProjectView = lazy(() => import("@/views/projects/EditProjectView"));
+const ProjectsDetailsView = lazy(() => import("@/views/projects/ProjectsDetailsView"));
+const LoginView = lazy(() => import("@/views/auth/LoginView"));
+const RegisterView = lazy(() => import("@/views/auth/RegisterView"));
+const ConfirmAccountView = lazy(() => import("@/views/auth/ConfirmAccountView"));
+const RequestNewCodeView = lazy(() => import("@/views/auth/RequestNewCodeView"));
+const ForgotPasswordView = lazy(() => import("@/views/auth/ForgotPasswordView"));
+const NewPasswordView = lazy(() => import("@/views/auth/NewPasswordView"));
+const ProjectTeamView = lazy(() => import("@/views/projects/ProjectTeamView"));
+const ProfileView = lazy(() => import("@/views/profile/ProfileView"));
+const ChangePasswordView = lazy(() => import("@/views/profile/ChangePasswordView"));
+const NotFound = lazy(() => import("@/views/404/NotFound"));
 
 export default function Router() {
   return (
     <BrowserRouter>
         <Routes>
             <Route element={<AppLayout/>}>
-              <Route path="/" element={<DashboardView/>} index></Route>
-              <Route path="/projects/create" element={<CreateProjectView/>}></Route>
-              <Route path="/projects/:projectId" element={<ProjectsDetailsView/>}></Route>
-              <Route path="/projects/:projectId/edit" element={<EditProjectView/>}></Route>
-              <Route path="/projects/:projectId/team" element={<ProjectTeamView/>}></Route>
+              <Route path="/" element={<Suspense><DashboardView/></Suspense>} index></Route>
+              <Route path="/projects/create" element={<Suspense><CreateProjectView/></Suspense>}></Route>
+              <Route path="/projects/:projectId" element={<Suspense><ProjectsDetailsView/></Suspense>}></Route>
+              <Route path="/projects/:projectId/edit" element={<Suspense><EditProjectView/></Suspense>}></Route>
+              <Route path="/projects/:projectId/team" element={<Suspense><ProjectTeamView/></Suspense>}></Route>
               <Route element={<ProfileLayout />}>
-                <Route path="/profile" element={<ProfileView/>}></Route>
-                <Route path="/profile/password" element={<ChangePasswordView/>}></Route>
+                <Route path="/profile" element={<Suspense><ProfileView/></Suspense>}></Route>
+                <Route path="/profile/password" element={<Suspense><ChangePasswordView/></Suspense>}></Route>
               </Route>
             </Route>
 
-            <Route element={<AtuhLayout />}>
-              <Route path="/auth/login" element={<LoginView />}></Route>
-              <Route path="/auth/register" element={<RegisterView />}></Route>
-              <Route path="/auth/confirm-account" element={<ConfirmAccountView />}></Route>
-              <Route path="/auth/request-code" element={<RequestNewCodeView />}></Route>
-              <Route path="/auth/forgot-password" element={<ForgotPasswordView />}></Route>
-              <Route path="/auth/new-password" element={<NewPasswordView />}></Route>
+            <Route element={<AuthLayout />}>
+              <Route path="/auth/login" element={<Suspense><LoginView/></Suspense>}></Route>
+              <Route path="/auth/register" element={<Suspense><RegisterView/></Suspense>}></Route>
+              <Route path="/auth/confirm-account" element={<Suspense><ConfirmAccountView/></Suspense>}></Route>
+              <Route path="/auth/request-code" element={<Suspense><RequestNewCodeView/></Suspense>}></Route>
+              <Route path="/auth/forgot-password" element={<Suspense><ForgotPasswordView/></Suspense>}></Route>
+              <Route path="/auth/new-password" element={<Suspense><NewPasswordView/></Suspense>}></Route>
+            </Route>
+
+            <Route element={<AuthLayout />}>
+              <Route path="*" element={<Suspense><NotFound/></Suspense>}></Route>
             </Route>
         </Routes>
-
-        <Route element={<AtuhLayout/>}>
-          <Route path="*" element={<NotFound />}></Route>
-        </Route>
     </BrowserRouter>
   )
 }
